846a4981aedd4e89c33c9dee8a779c31f489555a
adityash97 <adityash212@gmail.com>
2022-07-02 15:17:56 +0530

    using sql

--- pivony_backend/pivony_backend/settings.py
+++ pivony_backend/pivony_backend/settings.py
@@ -77,19 +77,19 @@ WSGI_APPLICATION = 'pivony_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'pivony',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'djongo',
+#         'NAME': 'pivony',
+#     }
+# }
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
